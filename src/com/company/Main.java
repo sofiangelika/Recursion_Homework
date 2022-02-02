55f3825dd02d37cb356dcef99c4ebc0634e397fe
sofi <sofia.com>
2022-02-02 17:32:01 +0200

    updates

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,11 +1,18 @@
 package com.company;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        int[] check = {0, 1, 2, 1, 0};
+        int[] check = {0, 1, 2, 1, 0, 33, 20, 21};
         System.out.println(palindrome(check, 0, check.length - 1));
+
+        System.out.println(recursiveSequential(check, 210, 0));
+
+        int[] bits = new int[4];
+        bitArrays(bits, 3);
 
     }
 
@@ -49,6 +56,18 @@ public class Main {
         }
     }
 
+    //Homework 4:
+    static boolean recursiveSequential(int[] arrayToCheck, int key, int index) {
+        if (index >= arrayToCheck.length)
+            return false;
+        if (arrayToCheck[index] == key)
+            return true;
+        else
+            return recursiveSequential(arrayToCheck, key, index + 1);
+
+
+    }
+
     //Homework 5:
     static boolean palindrome(int[] arrayToCheck, int start, int end) {
         if (start >= end)
@@ -56,5 +75,16 @@ public class Main {
         if (arrayToCheck[start] == arrayToCheck[end])
             return palindrome(arrayToCheck, start + 1, end - 1);
         return false;
+    }
+
+    //Homework 6:
+    static void bitArrays(int[] bits, int n) {
+        if (n < 0) {
+            System.out.println(java.util.Arrays.toString(bits));
+            return;
+        }
+        for (bits [n] = 0; bits [n] <= 1; bits [n]++) {
+            bitArrays(bits, n - 1);
+        }
     }
 }
